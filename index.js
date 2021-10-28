fdcb9bc7ed139f0ec97c7feae243b007f2d59141
Marzia Moon <marzia.moon9900@gmail.com>
2021-10-29 00:46:52 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express();
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
+
+
+
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e6utb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect()
+        console.log('database server tour and tourist is connected');
+    }
+    finally {
+        // await client.close()
+    }
+}
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send('welcome to tour and tourist')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
