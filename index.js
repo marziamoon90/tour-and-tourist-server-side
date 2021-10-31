32837f4f1cc30d711a567bd68aca2c3ae49efa75
Marzia Moon <marzia.moon9900@gmail.com>
2021-11-01 02:46:00 +0600

    all done

--- index.js
+++ index.js
@@ -33,6 +33,22 @@ async function run() {
             res.send(plans)
         });
 
+        //GET API FROM BOOKING
+        app.get('/booking', async (req, res) => {
+            const cursor = bookingCollection.find({});
+            const booking = await cursor.toArray();
+            // console.log(booking);
+            res.send(booking);
+        })
+
+        // ADD A PLAN TO DATABASE
+        app.post('/plans', async (req, res) => {
+            const plans = req.body;
+            console.log('plans added', plans);
+            const result = await planCollection.insertOne(plans);
+            res.json(result);
+        })
+
         // GET SINGLE DATA API 
         app.get('/plans/:id', async (req, res) => {
             const id = req.params.id;
@@ -42,13 +58,47 @@ async function run() {
             res.json(plan);
         })
 
+        // UPDATE API 
+        app.put('/booking/:id', async (req, res) => {
+            const id = req.params.id;
+            // console.log('get id', id)
+            const updatedStatus = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    status: updatedStatus.status
+                },
+            };
+            const result = await bookingCollection.updateOne(filter, updateDoc, options)
+            res.json(result)
+        })
+
         // ADD BOOKING PLAN 
         app.post('/booking', async (req, res) => {
             const booking = req.body;
             console.log('booking', booking);
-            const result = await bookingCollection.insertOne(booking)
+            const result = await bookingCollection.insertOne(booking);
+            res.json(result);
+        });
+
+        // DELETE API 
+        app.delete('/booking/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await bookingCollection.deleteOne(query)
             res.json(result)
         })
+
+
+        // Get User Booking Api by email
+        app.post('/booking/user', async (req, res) => {
+            const userEmail = req.body;
+            const query = { email: { $in: userEmail } };
+            const booked = await bookingCollection.find(query).toArray();
+            res.json(booked);
+        })
+
 
     }
     finally {
